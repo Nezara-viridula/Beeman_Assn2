124f8ed0baa1ed7fbd668ace27e04cc134967e54
Morgan Beeman <mcb0143@utulsa.edu>
2020-09-22 15:17:46 -0500

    Saving changes, able to grep ID numbers 13BTS10-89 using for loop

--- Beeman_Assn2.sh
+++ Beeman_Assn2.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+#creating child script
 touch childscript_beeman.sh
+
+#add shebang to childscript
+echo "#! /bin/bash" >> childscript_beeman.sh
+
+#grep data subset from BTS data.
+echo "for Num in {10..89}; do grep 13BTS0$'Num' BTS_data.txt; done >> beeman_output.txt" >> childscript_beeman.sh
+
 
 
